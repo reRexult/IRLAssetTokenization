93cbd2f7d2d30ece419855f32dc39d6d136e9936
Fleebers <90422222+reRexult@users.noreply.github.com>
2021-09-21 21:56:05 +0100

    token test and crowdsale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var TheAsset = artifacts.require("TheAsset");
+var AssetSale = artifacts.require("AssetSale");
 
 module.exports = async function(deployer) {
+	let addr = await web3.eth.getAccounts()
 	await deployer.deploy(TheAsset, 1000000);
+	await deployer.deploy(AssetSale, 1, addr[0], TheAsset.address)
+	let instance = await TheAsset.deployed();
+	await instance.transfer(AssetSale.address, 1000000);
 }
